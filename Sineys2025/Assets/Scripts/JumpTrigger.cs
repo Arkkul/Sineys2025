5aae46f7b7d6f60092bd536d576df2f845795b7d
Arkkul <kulaginarkadij1407@gmail.com>
2025-11-01 11:12:22 +0300

    ааа прыжок

--- Sineys2025/Assets/Scripts/JumpTrigger.cs
+++ Sineys2025/Assets/Scripts/JumpTrigger.cs
@@ -1,18 +1,38 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class JumpTrigger : MonoBehaviour
 {
     [SerializeField] private float _jumpForce = 5f;
+    [SerializeField] private float _jumpDuration = 0.5f;
+    [SerializeField] private AnimationCurve _jumpCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
 
     private void OnTriggerEnter(Collider other)
     {
-        
-        if(other.GetComponent<PigController>() != null)
+        if (other.GetComponent<PigController>() != null)
         {
             print(other.name);
-            //other.GetComponent<Rigidbody>().AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
-            other.transform.position = new Vector3(other.transform.position.x, other.transform.position.y + _jumpForce, other.transform.position.z);
+            StartCoroutine(SmoothJumpCoroutine(other.transform));
         }
+    }
+
+    private IEnumerator SmoothJumpCoroutine(Transform target)
+    {
+        Vector3 startPosition = target.position;
+        Vector3 endPosition = startPosition + Vector3.up * _jumpForce;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < _jumpDuration)
+        {
+            float progress = elapsedTime / _jumpDuration;
+            float curveValue = _jumpCurve.Evaluate(progress);
+
+            target.position = Vector3.Lerp(startPosition, endPosition, curveValue);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        target.position = endPosition;
     }
 }
