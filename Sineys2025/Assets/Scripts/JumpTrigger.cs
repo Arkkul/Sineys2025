96c755155e9a8be654a5b32cbb06f07b09097da4
Arkkul <kulaginarkadij1407@gmail.com>
2025-11-02 01:56:51 +0300

    прыгаем заново

--- Sineys2025/Assets/Scripts/JumpTrigger.cs
+++ Sineys2025/Assets/Scripts/JumpTrigger.cs
@@ -9,34 +9,34 @@ public class JumpTrigger : MonoBehaviour
     [SerializeField] private float _forwardJumpForce = 5f;
     [SerializeField] private AnimationCurve _jumpCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
 
+  
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PigController>() != null)
         {
             print(other.name);
-            StartCoroutine(PlatformIndependentJump(other.transform));
+            StartCoroutine(SmoothJumpUpOnly(other.transform));
         }
     }
 
-    private IEnumerator PlatformIndependentJump(Transform target)
+    private IEnumerator SmoothJumpUpOnly(Transform target)
     {
         Vector3 startPosition = target.position;
-        Vector3 endPosition = startPosition + Vector3.up * _jumpForce + target.transform.forward * _forwardJumpForce;
+        Vector3 endPosition = startPosition + Vector3.up * _jumpForce + target.transform.forward*_forwardJumpForce;
         float elapsedTime = 0f;
 
         while (elapsedTime < _jumpDuration)
         {
-            // Этот расчет будет одинаковым на любом компьютере
             float progress = elapsedTime / _jumpDuration;
-            float curveValue = _jumpCurve.Evaluate(progress);
+            // SmoothStep для плавного старта и остановки
+            float smoothProgress = Mathf.SmoothStep(0f, 1f, progress);
 
-            target.position = Vector3.Lerp(startPosition, endPosition, curveValue);
-
+            target.position = Vector3.Lerp(startPosition, endPosition, smoothProgress);
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
-        // Гарантируем точное конечное положение
+        // Фиксируем конечную позицию
         target.position = endPosition;
     }
 }
